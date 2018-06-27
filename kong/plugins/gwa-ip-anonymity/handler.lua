74990e58ad4d37e56fa8032d936dc1f68a0b45bd
Paul Austin <paul.austin@revolsys.com>
2018-06-27 14:05:28 -0700

    ip anon forwarded header

--- kong/plugins/gwa-ip-anonymity/handler.lua
+++ kong/plugins/gwa-ip-anonymity/handler.lua
@@ -2,24 +2,37 @@ local BasePlugin = require "kong.plugins.base_plugin"
 
 local BcGovIpAnonymousHandler = BasePlugin:extend()
 
+function anonymizeIps(ips)
+  if ips == nil then
+    return nil
+  else
+    -- ipv6
+    ips = ips:gsub('([%da-fA-F]*:[%da-fA-F:]*:)[%da-fA-F]+', '%10')
+    
+    -- ipv4
+    ips = ips:gsub('(%d+%.%d+%.%d+%.)%d+', '%10')
+    return ips
+  end
+end
+
+
+function anonymizeHeaderIps(name)
+  local ips = ngx.req.get_headers()[name];
+  ips = anonymizeIps(ips)
+  ngx.req.set_header(name, ips)
+  return ips
+end
+
 function BcGovIpAnonymousHandler:new()
   BcGovIpAnonymousHandler.super.new(self, "gwa-ip-anonymity")
 end
 
-
 function BcGovIpAnonymousHandler:access(conf)
   BcGovIpAnonymousHandler.super.access(self)
-  local origin = ngx.var.upstream_x_forwarded_for
-  origin = origin:gsub('%s+', '')
-  
-  local origins = {}
-  origin:gsub('([^,]+)', function(clientIp)
-    origins[#origins+1] = clientIp:gsub('([.:])%d+$', '%10')
-  end)
-
-  ngx.var.upstream_x_forwarded_for = table.concat(origins, ', ')
+  anonymizeHeaderIps('Forwarded')
+  ngx.var.upstream_x_forwarded_for = anonymizeHeaderIps('x-forwarded-for')
 end
 
-BcGovIpAnonymousHandler.PRIORITY = 0
+BcGovIpAnonymousHandler.PRIORITY = 10000
 
 return BcGovIpAnonymousHandler
