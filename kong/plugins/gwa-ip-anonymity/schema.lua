8956459ae41793a52fb9cc045ff366531b7447bb
Paul Austin <paul.austin@revolsys.com>
2019-04-15 09:24:53 -0700

    validation function rather than regex

--- kong/plugins/gwa-ip-anonymity/schema.lua
+++ kong/plugins/gwa-ip-anonymity/schema.lua
@@ -1,15 +1,37 @@
+local re_match     = ngx.re.match
+
+local function validate_ipv4(value) {
+  if value != nil then
+    local number = tonumber(value)
+    if number != nil and value >=0 and value <=255 then
+      return true
+    end
+  end
+  return false, string.format("'%s' is not a number in range 0..255", tostring(value))
+}
+
+local function validate_ipv6(value) {
+  if value != nil then
+    local number = tonumber(value)
+    if number != nil and value >=0 and value <=9999 then
+      return true
+    end
+  end
+  return false, string.format("'%s' is not a number in range 0..9999", tostring(value))
+}
+
 return {
   no_consumer = true,
   fields = {
     ipv4_mask = {
       type = "number",
       default = "0",
-      regex = "([:digit:]|[:digit:][:digit:]|1[:digit:][:digit:]|2[0-4][:digit:]|25[0-5])"
+      func = validate_ipv4
     },
     ipv6_mask = {
       type = "number",
       default = "0",
-      regex = "[:xdigit:]{1,4}"
+      func = validate_ipv6
     }
   }
 }
